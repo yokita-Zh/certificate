fe9cc9b45add798088b72692681c6f617362e4ba
yokita-Zh <yokita.zh@gmail.com>
2015-04-15 21:07:21 +0800

    demo

---
+++ Demo/Demo/QYAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  QYAppDelegate.h
+//  Demo
+//
+//  Created by qingyun on 15-3-5.
+//  Copyright (c) 2015年 hnqingyun.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QYAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
